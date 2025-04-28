2246cd353cee085df3da6e05188725fe7bc7f0c8
chanyas <chanyas@stud.ntnu.no>
2025-04-28 18:26:10 +0200

    js-endringer

---
+++ assets/scripts/gsapscript.js
@@ -0,0 +1,17 @@
+//gsap js for smooth scrolling 
+//Chanya 
+gsap.registerPlugin(ScrollToPlugin);
+
+document.addEventListener('DOMContentLoaded', function () {
+  const startButton = document.getElementById('startButton'); // Id: name of the button
+
+  if (startButton) {
+    startButton.addEventListener('click', function () {
+      const firstSection = document.querySelectorAll('section')[1]; // Select the first section after the header
+      if (firstSection) {
+        gsap.to(window, { duration: 1, scrollTo: { y: firstSection, offsetY: 0 }, ease: "power2.inOut" }); // this is the animation for the scroll
+      }
+    });
+  }
+});
+
